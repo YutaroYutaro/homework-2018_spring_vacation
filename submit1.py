60f04c264d291a4eb32ef7f33d323150bdc546a8
yutaroyutaro <yutaroyutaro172@gmail.com>
2018-02-25 19:06:24 +0900

    tag test

--- submit1.py
+++ submit1.py
@@ -9,8 +9,6 @@ PW = settings.PW
 
 with ShibbolethClient(ID, PW) as client:
     res = client.get('https://portal.student.kit.ac.jp/')
-
-    # print(res.text)
-    f = open('submit1.html', 'w') # 書き込みモードで開く
-    f.write(res.text) # 引数の文字列をファイルに書き込む
-    f.close() # ファイルを閉じる
+    f = open('submit1.html', 'w')
+    f.write(res.text)
+    f.close()
