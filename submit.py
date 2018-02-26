43f28ef1dc884029b752a58c337b1e5e98ce5ef0
yutaroyutaro <yutaroyutaro172@gmail.com>
2018-02-27 00:11:42 +0900

    tag v2.0

--- submit.py
+++ submit.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
+import bs4
 import requests
+import re
 from shibboleth_login import ShibbolethClient
 import settings
 
-ID = settings.ID
-PW = settings.PW
+# パースするテスト用ファイルをオープン
+with open('submit1.html', 'r') as f:
 
-with ShibbolethClient(ID, PW) as client:
-    res = client.get('https://portal.student.kit.ac.jp/')
-    f = open('submit1.html', 'w')
-    f.write(res.text)
-    f.close()
+    soup = bs4.BeautifulSoup(f, "lxml")
+
+    # (class="div... cat... notice_list_dl clearfix)毎にパース
+    notice_list = soup.find_all('dl', class_=re.compile('notice_list_dl clearfix'))
+
+    # パースしたものを改行と空白を削除して表示
+    for notice in notice_list:
+        print(notice.get_text().replace("\t", "").replace("\r\n", "").replace("\n", ""))
